c0c02cb8efa483609a4885c3f01b886d1e96d7f6
zsoli89 <jakabzsoli89@gmail.com>
2023-08-09 04:17:43 +0200

    git init

---
+++ src/main/java/hu/webuni/commonlib/CommonlibApplication.java
@@ -0,0 +1,13 @@
+package hu.webuni.commonlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonlibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/webuni/commonlib/CommonlibApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webuni.commonlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
