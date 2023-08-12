26d317fbffdb99aad8ba5d3762cb548558351bf6
zsoli89 <jakabzsoli89@gmail.com>
2023-08-12 16:55:53 +0200

    feat: add BaseResponseDto

---
+++ src/main/java/hu/webuni/commonlib/base/BaseResponseDto.java
@@ -0,0 +1,45 @@
+package hu.webuni.commonlib.base;
+
+import java.util.Objects;
+
+public class BaseResponseDto {
+
+    private Object response;
+
+    public BaseResponseDto() {
+    }
+
+    public BaseResponseDto(Object response) {
+        this.response = response;
+    }
+
+    public Object getResponse() {
+        return response;
+    }
+
+    public BaseResponseDto setResponse(Object response) {
+        this.response = response;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseResponseDto that = (BaseResponseDto) o;
+        return Objects.equals(response, that.response);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(response);
+    }
+
+    @Override
+    public String toString() {
+        return "BaseResponseDto{" +
+                "response=" + response +
+                '}';
+    }
+}
